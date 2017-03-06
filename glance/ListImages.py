7e75680ea84dac78196a118ec415d1374d788c41
Seoung-Soo, Lee <lssnow73@gmail.com>
2017-03-06 20:08:54 +0900

    List Images for OpenStack Glance Service by Python SDK

--- glance/ListImages.py
+++ glance/ListImages.py
@@ -1,17 +1,37 @@
 
-from os import environ as env
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+
+import keystoneclient.v3.client as ksclient
 import glanceclient.v2.client as glclient
-import keystoneclient.v2_0.client as ksclient
+import novaclient.client as nvclient
 
-keystone = ksclient.Client(auth_url=env['OS_AUTH_URL'],
-                         username=env['OS_USERNAME'],
-                         password=env['OS_PASSWORD'],
-                         tenant_name=env['OS_TENANT_NAME'],
-                         region_name=env['OS_REGION_NAME'])
+auth = v3.Password(auth_url='http://192.168.219.188:35357/v3',
+                   username='admin',
+                   password='admin',
+                   project_name='admin',
+                   user_domain_id='default',
+                   project_domain_id='default'
+                   )
 
-glance_endpoint = keystone.service_catalog.url_for(service_type='image')
-glance = glclient.Client(glance_endpoint, token=keystone.auth_token)
+sess = session.Session(auth=auth)
 
+keystone = ksclient.Client(session=sess)
+projects = keystone.projects.list()
+for project in projects:
+    print project
+
+glance = glclient.Client(session=sess)
 images = glance.images.list()
+for img in images:
+    print img
 
-print images
+nova = nvclient.Client('2.1', session=sess)
+flavors = nova.flavors.list()
+for flavor in flavors:
+    print flavor
+
+servers = nova.servers.list()
+for server in servers:
+    print server
+
