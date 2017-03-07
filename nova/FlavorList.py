91171f89517fac311be9724d59a8b339abe89125
Seoung-Soo, Lee <lssnow73@gmail.com>
2017-03-08 08:53:43 +0900

    List Images for OpenStack Glance Service by Python SDK

--- nova/FlavorList.py
+++
@@ -1,9 +0,0 @@
-from keystone.GetAuth import *
-import novaclient.client as novaclient
-
-sess = GetSession()
-nova = novaclient.Client('2.1', session=sess)
-flavors = nova.flavors.list()
-
-for flavor in flavors:
-    print flavor
